96b52fab340f355de12e86637b5b420a1e2e582f
David <andrianarivo.david@gmail.com>
2024-06-08 18:55:40 +0300

    Update swagger.js

--- backend/swagger.js
+++ backend/swagger.js
@@ -3,6 +3,10 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const options = {
   definition: {
     openapi: '3.0.0',
+    servers: [{
+      url: 'http://localhost:3000/api/v1',
+      description: 'Dev'
+    }],
     info: {
       title: 'Hello World',
       version: '1.0.0',
