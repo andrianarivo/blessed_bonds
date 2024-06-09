9f6d49b388660e22e3839d4156cf75f724464958
David <andrianarivo.david@gmail.com>
2024-06-09 19:05:18 +0300

    Delete + Update Tag

--- backend/controllers/tagController.js
+++ backend/controllers/tagController.js
@@ -35,7 +35,7 @@ exports.getTag = asyncHandler(async (req, res) => {
     }
   });
   if (!tag) {
-    res.status(404).send({error: 'Tag not found'}) 
+    return res.status(404).send({error: 'Tag not found'}) 
   }
   res.json({
     tag
@@ -51,10 +51,71 @@ exports.createTag = [
     if (!errors.isEmpty()) {
       return res.status(400).send({errors: errors.array()});
     }
-    const { name } = req.body;
+    const { name, bgColor = undefined, textColor = undefined } = req.body;
     const tag = await prisma.tag.create({
       data: {
-        name
+        name,
+        bgColor,
+        textColor
+      }
+    });
+    res.json({
+      tag
+    })
+  })
+]
+
+exports.deleteTag = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const tagToDelete = await prisma.tag.findUnique({
+    where: {
+      id: parseInt(id)
+    }
+  });
+  if(!tagToDelete) {
+    return res.status(404).send({error: 'Tag not found'});
+  }
+  const tag = await prisma.tag.delete({
+    where: {
+      id: parseInt(id)
+    }
+  });
+  res.json({
+    tag
+  })
+})
+
+exports.updateTag = [
+  body('name', 'Name has to be string').isString().isLength({min: 3}).escape().optional(),
+  body('bgColor').custom(isHexColor).withMessage('Invalid hexadecimal color code'),
+  body('textColor').custom(isHexColor).withMessage('Invalid hexadecimal color code'),
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send({errors: errors.array()});
+    }
+    const { id } = req.params;
+    const tagToUpdate = await prisma.tag.findUnique({
+      where: {
+        id: parseInt(id)
+      }
+    });
+    if(!tagToUpdate) {
+      return res.status(404).send({error: 'Tag not found'});
+    }
+    const { 
+      name = tagToUpdate.name, 
+      bgColor = tagToUpdate.bgColor, 
+      textColor = tagToUpdate.textColor 
+    } = req.body;
+    const tag = await prisma.tag.update({
+      data: {
+        name,
+        bgColor,
+        textColor
+      },
+      where: {
+        id: parseInt(id)
       }
     });
     res.json({
--- backend/routes/api/v1/tagRouter.js
+++ backend/routes/api/v1/tagRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const {listTag, getTag, createTag} = require('../../../controllers/tagController');
+const {listTag, getTag, createTag, deleteTag, updateTag} = require('../../../controllers/tagController');
 
 /**
  * @openapi
@@ -79,5 +79,52 @@ router.get('/:id', getTag);
  *         description: Validation error
  */
 router.post('/', createTag);
+
+/**
+ * @openapi
+ * /tags/{id}:
+ *   delete:
+ *     description: Delete Tag specified by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: ID of the Tag
+ *     responses:
+ *       200:
+ *         description: Tag deleted successfully
+ *       404:
+ *         description: Tag not found
+ */
+router.delete('/:id', deleteTag);
+
+/**
+ * @openapi
+ * /tags/{id}:
+ *   patch:
+ *     description: Update Tag specified by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: ID of the Tag
+ *     requestBody:
+ *       description: Tag data
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *            $ref: '#/components/schemas/Tag'
+ *     responses:
+ *       200:
+ *         description: Tag updated successfully
+ *       400:
+ *         description: Validation error
+ */
+router.patch('/:id', updateTag);
 
 module.exports = router;
