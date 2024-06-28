8d476705d79b759beba697fdb202bae1c12f6d64
David <andrianarivo.david@gmail.com>
2024-06-28 18:32:36 +0300

    refactor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world?</h1>
-      <button className="btn">Button</button>
+      <button type="button" className="btn">
+        Button
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui'
+import daisyui from 'daisyui';
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -7,4 +7,4 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
-}
+};
