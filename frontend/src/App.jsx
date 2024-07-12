d6977bcd43e71a0a2805dffccca6f30150d64aee
David <andrianarivo.david@gmail.com>
2024-07-12 21:46:55 +0300

    migrate to mdxeditor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import './App.css';
+import {
+  MDXEditor,
+  headingsPlugin,
+  listsPlugin,
+  toolbarPlugin,
+  markdownShortcutPlugin,
+  linkDialogPlugin,
+  linkPlugin,
+  UndoRedo,
+  BoldItalicUnderlineToggles,
+  CreateLink,
+} from '@mdxeditor/editor';
 import Tag from './components/Tag';
 import Note from './components/Note';
 import Prayer from './components/Prayer';
-import Editor from './components/Editor';
+import '@mdxeditor/editor/style.css';
 
 const tags = [
   {
@@ -81,8 +93,28 @@ const App = () => (
       />
     </div>
 
-    <div className="w-2/3">
-      <Editor />
+    <div className="w-2/3 border border-gray-400">
+      <MDXEditor
+        contentEditableClassName="markdown"
+        markdown="# Hello world"
+        plugins={[
+          headingsPlugin(),
+          listsPlugin(),
+          markdownShortcutPlugin(),
+          linkDialogPlugin(),
+          linkPlugin(),
+          toolbarPlugin({
+            toolbarContents: () => (
+              <>
+                {' '}
+                <UndoRedo />
+                <BoldItalicUnderlineToggles />
+                <CreateLink />
+              </>
+            ),
+          }),
+        ]}
+      />
     </div>
   </div>
 );
