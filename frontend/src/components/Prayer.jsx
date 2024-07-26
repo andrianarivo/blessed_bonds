95f5e1c0074e46e0e306c84bd9e2bf43c300dbd4
David <andrianarivo.david@gmail.com>
2024-07-26 18:31:38 +0300

    feat prayer

--- frontend/src/components/Prayer.jsx
+++ frontend/src/components/Prayer.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
+import React, { Children } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import classNames from 'classnames';
-import { Swiper, SwiperSlide } from 'swiper/react';
 import Avatar from './Avatar';
-import SwiperPrevButton from './swipper/SwiperPrevButton';
-import SwiperNextButton from './swipper/SwiperNextButton';
 
 import 'swiper/css';
 
@@ -14,16 +11,14 @@ const Prayer = ({
   description,
   createdAt,
   iconUrl = undefined,
-  noteCount = 0,
+  isLargeDisplay = false,
+  notesCount = 0,
   answersCount = 0,
-  tags = [],
-  notes = [],
-  answers = [],
   author = 'anonymous',
-  renderTags = undefined,
-  renderNotes = undefined,
-  renderAnswers = undefined,
-  renderEditor = undefined,
+  tags = null,
+  notes = null,
+  answers = null,
+  editor = null,
   onClickSeeNotes = undefined,
 }) => {
   const cardClass = classNames(
@@ -42,9 +37,6 @@ const Prayer = ({
     'line-clamp-4': !notes || notes.length <= 0,
   });
 
-  const isLargeDisplay =
-    renderNotes && Array.isArray(notes) && notes.length > 0;
-
   const dropdownClass = classNames('dropdown', 'dropdown-end', {
     'dropdown-top': !isLargeDisplay,
   });
@@ -57,51 +49,16 @@ const Prayer = ({
     'backdrop-blur-md',
     'rounded-xl',
     {
-      'w-96': Array.isArray(answers) && answers.length > 0,
+      'w-96': Children.count(answers) > 0,
     },
     {
-      'w-48':
-        !Array.isArray(answers) ||
-        (Array.isArray(answers) && answers.length <= 0),
+      'w-48': Children.count(answers) <= 0,
     }
   );
 
-  const swiperCtaClass = classNames('flex', 'justify-between', 'my-2', {
-    invisible:
-      !Array.isArray(answers) ||
-      (Array.isArray(answers) && answers.length <= 0),
-  });
-
-  const renderAnswersList = () => (
+  const renderAnswers = () => (
     <div className={dropdownContentClass}>
-      {Array.isArray(answers) && answers.length > 0 ? (
-        <Swiper slidesPerView={1}>
-          {renderAnswers &&
-            answers.map((answer, index) => (
-              <SwiperSlide key={answer.id}>
-                {renderAnswers(answer, index)}
-              </SwiperSlide>
-            ))}
-          <div className={swiperCtaClass}>
-            <SwiperPrevButton />
-            <div className="flex gap-2">
-              <button type="button" className="btn btn-circle bg-blue-600">
-                <span className="material-symbols-outlined text-2xl text-white">
-                  edit
-                </span>
-              </button>
-              <button type="button" className="btn btn-circle bg-orange-500">
-                <span className="material-symbols-outlined text-2xl text-white">
-                  delete
-                </span>
-              </button>
-            </div>
-            <SwiperNextButton />
-          </div>
-        </Swiper>
-      ) : (
-        <p className="text-center">Nothing to show here...</p>
-      )}
+      {Children.count(answers) > 0 ? answers : <p>Nothing to show here...</p>}
     </div>
   );
 
@@ -109,12 +66,7 @@ const Prayer = ({
     <div className={cardClass}>
       <div className="w-1 h-16 bg-indigo-600 absolute top-14 -left-[2px] rounded" />
       <div className="card-body p-5">
-        <ul className="flex flex-wrap gap-2">
-          {renderTags &&
-            Array.isArray(tags) &&
-            tags.length > 0 &&
-            tags.map((tag) => <li key={tag.id}>{renderTags(tag)}</li>)}
-        </ul>
+        {tags}
         <h2 className="card-title">{summary}</h2>
         <p className={descriptionClass}>{description}</p>
         <div className="card-actions flex-col">
@@ -127,7 +79,7 @@ const Prayer = ({
               <span className="material-symbols-outlined text-base">
                 description
               </span>
-              {`${noteCount} notes`}
+              {`${notesCount} notes`}
             </button>
             <div className={dropdownClass}>
               <div
@@ -140,14 +92,14 @@ const Prayer = ({
                 </span>
                 {`${answersCount} answers`}
               </div>
-              {renderAnswersList()}
+              {renderAnswers()}
             </div>
           </div>
           <div className="divider my-0" />
           {isLargeDisplay && (
-            <div className="w-full">
-              <div>{notes.map((note, idx) => renderNotes(note, idx))}</div>
-              <div className="my-4">{renderEditor && renderEditor()}</div>
+            <div className="w-full gap-4">
+              {notes}
+              {editor}
             </div>
           )}
 
@@ -167,7 +119,7 @@ const Prayer = ({
                     >
                       See answers
                     </div>
-                    {renderAnswersList()}
+                    {renderAnswers()}
                   </div>
 
                   <div className="rounded w-[3px] h-[3px] bg-gray-500" />
@@ -189,39 +141,15 @@ Prayer.propTypes = {
   summary: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   createdAt: PropTypes.string.isRequired,
-  noteCount: PropTypes.number,
+  isLargeDisplay: PropTypes.bool,
+  notesCount: PropTypes.number,
   answersCount: PropTypes.number,
   author: PropTypes.string,
   iconUrl: PropTypes.string,
-  tags: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      label: PropTypes.string.isRequired,
-      backgroundColor: PropTypes.string,
-      color: PropTypes.string,
-    })
-  ),
-  notes: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-      content: PropTypes.string.isRequired,
-      createdAt: PropTypes.string.isRequired,
-      isPrivate: PropTypes.bool,
-    })
-  ),
-  answers: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-      content: PropTypes.string.isRequired,
-      createdAt: PropTypes.string.isRequired,
-    })
-  ),
-  renderTags: PropTypes.func,
-  renderNotes: PropTypes.func,
-  renderAnswers: PropTypes.func,
-  renderEditor: PropTypes.func,
+  tags: PropTypes.element,
+  notes: PropTypes.element,
+  answers: PropTypes.element,
+  editor: PropTypes.func,
   onClickSeeNotes: PropTypes.func,
 };
 
--- frontend/src/stories/Prayer.stories.jsx
+++ frontend/src/stories/Prayer.stories.jsx
@@ -1,123 +1,82 @@
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable import/no-extraneous-dependencies */
+
 import { fn } from '@storybook/test';
+
 import Prayer from '../components/Prayer';
 import Tag from '../components/Tag';
 import Note from '../components/Note';
-import Avatar from '../components/Avatar';
-import Editor from '../components/Editor';
 import Answer from '../components/Answer';
+import TagRow from '../components/TagRow';
+import AnswerCarousel from '../components/AnswerCarousel';
+import NoteBox from '../components/NoteBox';
+
+import { Note1, Note2 } from './Note.stories';
+import { Answer1, Answer2 } from './Answer.stories';
+import { Tag1, Tag2 } from './Tag.stories';
+import MarkdownEditor from '../components/MarkdownEditor';
 
 export default {
-  title: 'Example/Prayer',
+  title: 'Playground/Prayer',
   component: Prayer,
+  subcomponents: { Tag },
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
   argTypes: {
-    summary: { control: 'text' },
-    description: { control: 'text' },
     createdAt: { control: 'date' },
-    iconUrl: { control: 'text' },
-    author: { control: 'text' },
-    noteCount: { control: 'number' },
-    answersCount: { control: 'number' },
-    tags: { control: 'object' },
+    notes: { table: { disable: true } },
+    answers: { table: { disable: true } },
+    tags: { table: { disable: true } },
+    onClickSeeNotes: { table: { disable: true } },
+    editor: { table: { disable: true } },
   },
 };
 
-const tags = [
-  {
-    id: 1,
-    label: 'Finance',
-    backgroundColor: '#ffece1',
-    color: '#ff5c00',
-  },
-  {
-    id: 2,
-    label: 'Self-control',
-    backgroundColor: '#e1f6ff',
-    color: '#2c62b4',
-  },
-];
-
-const notes = [
-  {
-    id: 1,
-    title: 'I hear the Gospel in my dreams',
-    content:
-      'Hi, *Pluto*! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et dui id leo suscipit imperdiet.',
-    isPrivate: false,
-    createdAt: '2021-09-01T00:00:00.000Z',
-  },
-  {
-    id: 2,
-    title: 'Jesus is the Messiah',
-    content:
-      'Hi, *Pluto*! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et dui id leo suscipit imperdiet.',
-    isPrivate: false,
-    createdAt: '2021-09-01T00:00:00.000Z',
-  },
-];
-
-const answers = [
-  {
-    id: 1,
-    title: 'Be patient ⏰',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et dui id leo suscipit imperdiet. Nunc congue mauris id felis pellentesque, non vulputate felis pellentesque.',
-  },
-  {
-    id: 2,
-    title: 'Gear up ⚙️',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et dui id leo suscipit imperdiet. Nunc congue mauris id felis pellentesque, non vulputate felis pellentesque.',
-  },
-];
-
-export const Example1 = {
+export const Expanded = {
+  render: (args) => (
+    <Prayer
+      tags={
+        <TagRow>
+          <Tag {...Tag1.args} />
+          <Tag {...Tag2.args} />
+        </TagRow>
+      }
+      answers={
+        <AnswerCarousel>
+          <Answer {...Answer1.args} />
+          <Answer {...Answer2.args} />
+        </AnswerCarousel>
+      }
+      notes={
+        <NoteBox>
+          <Note {...Note1.args} />
+          <Note {...Note2.args} />
+        </NoteBox>
+      }
+      editor={
+        <MarkdownEditor
+          author={args.author}
+          markdown={'### Note Title\nWrite a note...'}
+        />
+      }
+      {...args}
+    />
+  ),
   args: {
     summary: 'God is provider',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar urna vel efficitur iaculis. Praesent dapibus arcu et leo malesuada, in suscipit ligula tincidunt. Mauris malesuada lacinia eros. Proin sed maximus leo. Suspendisse at purus ac libero volutpat consequat.',
-    noteCount: 2,
+    notesCount: 2,
     answersCount: 2,
     author: 'David Stanley',
+    isLargeDisplay: true,
     createdAt: '2021-09-01T00:00:00.000Z',
-    tags,
-    notes,
-    answers,
-    renderTags: (tag) => (
-      <Tag
-        label={tag.label}
-        backgroundColor={tag.backgroundColor}
-        color={tag.color}
-        key={tag.label}
-      />
-    ),
-    renderNotes: (note, idx) => (
-      <Note
-        title={note.title}
-        text={note.content}
-        isOwn={idx % 2 === 0}
-        createdAt={note.createdAt}
-      />
-    ),
-    renderAnswers: (answer) => (
-      <Answer title={answer.title} content={answer.content} />
-    ),
-    renderEditor: () => (
-      <div className="flex gap-3">
-        <Avatar author="David Stanley" className="self-end" size="lg" />
-        <div className="flex-1 overflow-scroll">
-          <Editor content={'### Note Title\nWrite a note...'} />
-        </div>
-      </div>
-    ),
   },
 };
 
-export const Example2 = {
+export const Collapsed = {
   args: {
     summary: 'God is provider',
     description:
@@ -128,14 +87,5 @@ export const Example2 = {
     createdAt: '2021-09-01T00:00:00.000Z',
     onClickSeeNotes: fn(),
     answers: [],
-    tags,
-    renderTags: (tag) => (
-      <Tag
-        label={tag.label}
-        backgroundColor={tag.backgroundColor}
-        color={tag.color}
-        key={tag.label}
-      />
-    ),
   },
 };
