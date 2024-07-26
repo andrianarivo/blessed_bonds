355a99e4d10820bc278881fe4659eddc59f4eafe
David <andrianarivo.david@gmail.com>
2024-07-26 18:30:08 +0300

    feat answer

--- frontend/src/components/Answer.jsx
+++ frontend/src/components/Answer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Answer = ({ title, content }) => (
+const Answer = ({ title, content, onEdit, onRemove }) => (
   <div
     tabIndex="0"
     role="button"
@@ -9,12 +9,34 @@ const Answer = ({ title, content }) => (
   >
     <div className="text-md font-medium">{title}</div>
     <p className="font-normal text-sm text-gray-400">{content}</p>
+    <div className="flex gap-2 self-end">
+      <button
+        type="button"
+        className="btn btn-circle bg-blue-600"
+        onClick={onEdit}
+      >
+        <span className="material-symbols-outlined text-2xl text-white">
+          edit
+        </span>
+      </button>
+      <button
+        type="button"
+        className="btn btn-circle bg-orange-500"
+        onClick={onRemove}
+      >
+        <span className="material-symbols-outlined text-2xl text-white">
+          delete
+        </span>
+      </button>
+    </div>
   </div>
 );
 
 Answer.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
+  onEdit: PropTypes.func,
+  onRemove: PropTypes.func,
 };
 
 export default Answer;
---
+++ frontend/src/stories/Answer.stories.jsx
@@ -0,0 +1,26 @@
+import Answer from '../components/Answer';
+
+export default {
+  title: 'Components/Answer',
+  component: Answer,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export const Answer1 = {
+  args: {
+    title: 'Be patient ⏰',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et dui id leo suscipit imperdiet. Nunc congue mauris id felis pellentesque, non vulputate felis pellentesque.',
+  },
+};
+
+export const Answer2 = {
+  args: {
+    title: 'Gear up ⚙️',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et dui id leo suscipit imperdiet. Nunc congue mauris id felis pellentesque, non vulputate felis pellentesque.',
+  },
+};
