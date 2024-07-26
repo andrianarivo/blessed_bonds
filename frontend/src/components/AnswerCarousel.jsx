12cec61cbfcb11b808401268852c3fd7c31bbc18
David <andrianarivo.david@gmail.com>
2024-07-26 18:31:45 +0300

    feat answer

---
+++ frontend/src/components/AnswerCarousel.jsx
@@ -0,0 +1,30 @@
+import React, { Children } from 'react';
+import PropTypes from 'prop-types';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import classNames from 'classnames';
+import SwiperPrevButton from './swipper/SwiperPrevButton';
+import SwiperNextButton from './swipper/SwiperNextButton';
+
+const AnswerCarousel = ({ children }) => {
+  const swiperCtaClass = classNames('flex', 'justify-between', 'my-2', {
+    invisible: Children.count(children) <= 0,
+  });
+
+  return (
+    <Swiper slidesPerView={1}>
+      {Children.map(children, (child) => (
+        <SwiperSlide>{child}</SwiperSlide>
+      ))}
+      <div className={swiperCtaClass}>
+        <SwiperPrevButton />
+        <SwiperNextButton />
+      </div>
+    </Swiper>
+  );
+};
+
+AnswerCarousel.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
+};
+
+export default AnswerCarousel;
