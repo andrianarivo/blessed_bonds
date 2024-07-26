669ebd545df929d2b631ffb3a34cc1912e684fbf
David <andrianarivo.david@gmail.com>
2024-07-26 18:31:56 +0300

    update playground

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import Tag from './components/Tag';
 import Note from './components/Note';
 import Prayer from './components/Prayer';
-import Avatar from './components/Avatar';
-import Editor from './components/Editor';
 import Answer from './components/Answer';
+import TagRow from './components/TagRow';
+import AnswerCarousel from './components/AnswerCarousel';
+import NoteBox from './components/NoteBox';
+import MarkdownEditor from './components/MarkdownEditor';
 
 const tags = [
   {
@@ -63,59 +65,60 @@ const App = () => {
   return (
     <div className="flex flex-col justify-center items-center gap-y-4">
       <h1 className="text-2xl my-6">Playground phase</h1>
-      <div className="flex">
-        <Tag label="Finance" backgroundColor="#ffece1" color="#ff5c00" />
-      </div>
-
-      <div className="w-2/3">
-        <Note
-          title="I hear the Gospel in my dreams"
-          text="# Hi, *Pluto*! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et dui id leo suscipit imperdiet. "
-          author="David Stanley"
-          isOwn={false}
-          createdAt="2021-09-01T00:00:00.000Z"
-        />
-      </div>
 
       <div className="w-2/3">
         <Prayer
           summary="God is provider"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar urna vel efficitur iaculis. Praesent dapibus arcu et leo malesuada, in suscipit ligula tincidunt. Mauris malesuada lacinia eros. Proin sed maximus leo. Suspendisse at purus ac libero volutpat consequat."
+          isLargeDisplay
           noteCount={2}
           answersCount={2}
           author="David Stanley"
           createdAt="2021-09-01T00:00:00.000Z"
-          tags={tags}
-          renderTags={(tag, idx) => (
-            <Tag
-              key={idx}
-              label={tag.label}
-              backgroundColor={tag.backgroundColor}
-              color={tag.color}
+          tags={
+            <TagRow>
+              {tags.map((tag) => (
+                <Tag
+                  key={tag.id}
+                  label={tag.label}
+                  backgroundColor={tag.backgroundColor}
+                  color={tag.color}
+                />
+              ))}
+            </TagRow>
+          }
+          notes={
+            <NoteBox>
+              {notes.map((note, idx) => (
+                <Note
+                  key={note.id}
+                  title={note.title}
+                  content={note.content}
+                  createdAt={note.createdAt}
+                  author={note.author}
+                  isOwn={idx % 2 === 0}
+                />
+              ))}
+            </NoteBox>
+          }
+          answers={
+            <AnswerCarousel>
+              {answers.map((answer) => (
+                <Answer
+                  key={answer.id}
+                  title={answer.title}
+                  content={answer.content}
+                />
+              ))}
+            </AnswerCarousel>
+          }
+          editor={
+            <MarkdownEditor
+              author="David Stanley"
+              markdown={markdown}
+              setMarkdown={setMarkdown}
             />
-          )}
-          notes={notes}
-          renderNotes={(note, idx) => (
-            <Note
-              key={idx}
-              title={note.title}
-              text={note.content}
-              isOwn={idx % 2 === 0}
-              createdAt={note.createdAt}
-            />
-          )}
-          answers={answers}
-          renderAnswers={(answer) => (
-            <Answer title={answer.title} content={answer.content} />
-          )}
-          renderEditor={() => (
-            <div className="flex gap-3">
-              <Avatar author="David Stanley" className="self-end" size="lg" />
-              <div className="flex-1 overflow-scroll">
-                <Editor content={markdown} onChange={setMarkdown} />
-              </div>
-            </div>
-          )}
+          }
         />
       </div>
     </div>
