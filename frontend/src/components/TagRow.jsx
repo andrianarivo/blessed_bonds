2f7dd962a3e07a6d847e7657fc427d1a30d07235
David <andrianarivo.david@gmail.com>
2024-07-29 19:46:03 +0300

    tag row list none

--- frontend/src/components/TagRow.jsx
+++ frontend/src/components/TagRow.jsx
@@ -2,7 +2,7 @@ import React, { Children } from 'react';
 import PropTypes from 'prop-types';
 
 const TagRow = ({ children }) => (
-  <ul className="flex flex-wrap gap-2">
+  <ul className="flex flex-wrap gap-2 list-none">
     {Children.map(children, (child) => (
       <li>{child}</li>
     ))}
