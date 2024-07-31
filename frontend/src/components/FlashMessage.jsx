59ac84537f0ee61797b9f8c684c4b5c7bf3bef63
David <andrianarivo.david@gmail.com>
2024-07-31 19:49:49 +0300

    feat flash message

---
+++ frontend/src/components/FlashMessage.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cl from 'classnames';
+
+const FlashMessage = ({ classNames }) => {
+  const wrapperClass = cl(classNames);
+  return (
+    <div className={wrapperClass}>
+      <div className="relative flex flex-col justify-center items-center bg-gray-100 rounded-xl p-4 gap-4">
+        <div className="absolute -top-8 p-4 bg-gray-100 rounded-full">
+          <span className="material-symbols-outlined text-yellow-500">
+            lightbulb
+          </span>
+        </div>
+        <p className="text-sm font-medium z-20">Thoughts Time</p>
+        <p className="text-xs text-center text-gray-400">
+          We don’t have any notice for you, till then you can share your
+          thoughts with your peers.
+        </p>
+        <button type="button" className="btn bg-white font-medium">
+          Write a message
+        </button>
+      </div>
+    </div>
+  );
+};
+
+FlashMessage.propTypes = {
+  classNames: PropTypes.string,
+};
+
+export default FlashMessage;
