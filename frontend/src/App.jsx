a6bd4783d385b736d5b17fbbea348a64ef02d543
David <andrianarivo.david@gmail.com>
2024-08-16 18:40:26 +0300

    design implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,21 +22,99 @@ const App = () => (
             htmlFor="app-drawer"
             className="btn btn-ghost drawer-button lg:hidden"
           >
-            <span className="material-symbols-outlined text-gray-400">
-              menu
-            </span>
+            <span className="material-symbols-rounded text-gray-400">menu</span>
             <input hidden type="checkbox" />
           </label>
         }
       />
 
-      <div className="w-full">
-        <Users>
-          <Avatar author="David Stanley" />
-          <Avatar author="David Stanley" />
-          <Avatar author="David Stanley" />
-          <Avatar author="David Stanley" />
-        </Users>
+      <div className="w-full p-4">
+        <button
+          className="btn btn-square rounded-2xl btn-lg bg-indigo-600 text-white absolute bottom-4 right-4"
+          type="button"
+        >
+          <span className="material-symbols-rounded text-6xl">add</span>
+        </button>
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center justify-between">
+            <div className="flex items-baseline">
+              <p className="text-4xl tracking-wide">Career</p>
+              <div className="flex gap-4 ml-32">
+                <button
+                  className="btn btn-xs btn-square bg-purple-100 text-purple-400"
+                  type="button"
+                >
+                  <span className="material-symbols-rounded text-sm">edit</span>
+                </button>
+                <button
+                  className="btn btn-xs btn-square bg-purple-100 text-purple-400"
+                  type="button"
+                >
+                  <span className="material-symbols-rounded text-sm">link</span>
+                </button>
+              </div>
+            </div>
+            <Users>
+              <Avatar author="David Stanley" />
+              <Avatar author="David Stanley" />
+              <Avatar author="David Stanley" />
+              <Avatar author="David Stanley" />
+            </Users>
+          </div>
+          <div className="flex items-center justify-between mt-4">
+            <div className="flex gap-4">
+              <button
+                className="btn btn-outline btn-sm border-gray-300 text-gray-400"
+                type="button"
+              >
+                <span className="material-symbols-rounded text-sm">
+                  filter_alt
+                </span>
+                Filter
+                <span className="material-symbols-rounded text-gray-400">
+                  keyboard_arrow_down
+                </span>
+              </button>
+              <button
+                className="btn btn-outline btn-sm border-gray-300 text-gray-400"
+                type="button"
+              >
+                <span className="material-symbols-rounded text-sm">today</span>
+                Today
+                <span className="material-symbols-rounded text-gray-400">
+                  keyboard_arrow_down
+                </span>
+              </button>
+            </div>
+            <div className="divide-x flex gap-4">
+              <button
+                className="btn btn-outline btn-sm border-gray-300 text-gray-400"
+                type="button"
+              >
+                <span className="material-symbols-rounded text-sm">share</span>
+                Share
+              </button>
+              <div className="flex gap-4">
+                <button
+                  className="btn btn-sm btn-square btn-ghost btn-active ml-4"
+                  type="button"
+                >
+                  <span className="material-symbols-rounded symbols-filled  text-sm">
+                    view_agenda
+                  </span>
+                </button>
+                <button
+                  className="btn btn-sm btn-square btn-ghost mr-4"
+                  type="button"
+                >
+                  <span className="material-symbols-rounded symbols-filled  text-sm">
+                    grid_view
+                  </span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
@@ -53,7 +131,7 @@ const App = () => (
           <Logo />
           <MenuSection>
             <MenuItem icon="business_center" />
-            <MenuItem icon="priority" title="answers" />
+            <MenuItem icon="check_circle_outline" title="answers" />
             <MenuItem icon="wysiwyg" title="notes" />
           </MenuSection>
           <MenuSection title="my topics" canAddMore>
