9a52fcbcd23feb03b29a55d0ca87aad94b840590
David <andrianarivo.david@gmail.com>
2024-08-24 19:28:29 +0300

    feat Status

--- frontend/src/components/Status.jsx
+++ frontend/src/components/Status.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Status = () => (
-  <div className="min-w-96 rounded-tl-2xl rounded-tr-2xl bg-gray-100 p-4">
-    <div className="flex items-center gap-2">
-      <div className="w-2 h-2 rounded-full bg-orange-600" />
-      <p className="font-medium">Vision</p>
-      <div className="w-5 h-5 rounded-full bg-gray-300 text-xs flex justify-center items-center">
-        1
-      </div>
+const Status = ({ title, badge = 0, color = '#5030e5', children }) => (
+  <div className="w-status rounded-tl-2xl rounded-tr-2xl bg-gray-100 pt-4 flex flex-col shrink-0">
+    <div className="flex items-center gap-2 mx-4">
+      <div
+        className="w-2 h-2 rounded-full"
+        style={{ backgroundColor: color }}
+      />
+      <p className="font-medium">{title}</p>
+      {badge > 0 && (
+        <div className="badge badge-ghost bg-gray-300">{badge}</div>
+      )}
     </div>
-    <div className="h-[2px] w-full my-4 bg-orange-600" />
+    <div className="h-[2px] my-4 mx-4" style={{ backgroundColor: color }} />
+    <div className="flex-1 overflow-auto flex flex-col gap-2 items-center pb-4">
+      {children}
+    </div>
   </div>
 );
 
-Status.propTypes = {};
+Status.propTypes = {
+  title: PropTypes.string.isRequired,
+  badge: PropTypes.number,
+  color: PropTypes.string,
+  children: PropTypes.element,
+};
 
 export default Status;
