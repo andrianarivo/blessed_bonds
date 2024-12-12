1e456543e6922d175c0db8e40f4c51e955905933
David <andrianarivo.david@gmail.com>
2024-12-12 20:07:26 +0300

    update config

--- backend/src/features/resolvers.ts
+++ backend/src/features/resolvers.ts
@@ -1,13 +1,17 @@
 import { Resolvers } from "@/__generated__/graphql";
-import * as helloWorldMutations from "@/features/me/mutations";
+import * as uploadProfilePicture from "@/features/me/mutations";
 import * as me from "@/features/me/queries";
+import * as signIn from "@/features/sign-in/mutations";
+import * as signUp from "@/features/sign-up/mutations";
 
 const Query = {
   ...me,
 };
 
 const Mutation = {
-  ...helloWorldMutations,
+  ...uploadProfilePicture,
+  ...signIn,
+  ...signUp,
 };
 
 export const resolvers: Resolvers = {
