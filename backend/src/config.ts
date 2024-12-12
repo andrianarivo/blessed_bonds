e6ddc41bb5bb808094f4d913c9ebb75e6415253d
David <andrianarivo.david@gmail.com>
2024-12-12 20:05:41 +0300

    update config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -8,12 +8,26 @@ const envSchema = z.object({
   NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
   HOSTNAME: z.string().default("localhost"),
   FRONTEND_URL: z.string().default("http://localhost:5173"),
-  GOOGLE_CLIENT_ID: z.string(),
-  GOOGLE_CLIENT_SECRET: z.string(),
   SESSION_SECRET: z.string(),
   JWT_SECRET: z.string(),
+
+  // Google
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+
+  // Facebook
   FACEBOOK_APP_ID: z.string(),
   FACEBOOK_APP_SECRET: z.string(),
+
+  // Storage
+  STORAGE_TYPE: z.enum(["local", "aws", "gcp"]).default("local"),
+  UPLOAD_DIR: z.string().default("uploads"),
+
+  // AWS
+  AWS_BUCKET: z.string(),
+  AWS_REGION: z.string(),
+  AWS_ACCESS_KEY: z.string(),
+  AWS_SECRET_KEY: z.string(),
 });
 
 export const config = envSchema.parse(process.env);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,11 +7,18 @@ import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHt
 import { User } from "@prisma/client";
 import cors from "cors";
 import express from "express";
+import { graphqlUploadExpress } from "graphql-upload-ts";
 import http from "http";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 import { configureAuth } from "./auth/middleware";
 import { verifyToken } from "./auth/verify_token";
 import { prisma } from "./db";
 import { Context } from "./types/context";
+
+// because ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 async function startApolloServer() {
   const app = express();
@@ -34,6 +41,7 @@ async function startApolloServer() {
       credentials: true,
     }),
     express.json(),
+    graphqlUploadExpress(),
     expressMiddleware<Context>(server, {
       context: async ({ req }) => {
         if (req.user) {
@@ -54,6 +62,8 @@ async function startApolloServer() {
       },
     })
   );
+
+  app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 
   await new Promise<void>((resolve) => httpServer.listen({ port: config.PORT }, resolve));
   console.log(`🚀 Server ready at http://localhost:${config.PORT}/graphql`);
